4e9ab6c9c0e026a2d923c3e63e13efcf25b7e247
zihao.liu <zihao.liu@asiainnovations.com>
2015-09-10 17:57:50 +0800

    add user pref

--- GropUpEmails/GroupUpEmails.cs
+++ GropUpEmails/GroupUpEmails.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Data;
 using System.Data.OleDb;
-using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Net.Mail;
-using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 using System.Xml;
 using GropUpEmails.Properties;
-using Microsoft.Office.Interop.Excel;
 using Application = System.Windows.Forms.Application;
 using DataTable = System.Data.DataTable;
 
@@ -18,7 +15,7 @@ namespace GropUpEmails
 {
     public partial class GroupUpEmails : Form
     {
-        private const string STYLE_TITLE =
+        private const string STYLE_HEADER =
             "HEIGHT: 23.25pt; " +
             "WIDTH: 92pt; " +
             "BORDER-TOP: windowtext 0.5pt solid;" +
@@ -26,9 +23,25 @@ namespace GropUpEmails
             "BORDER-BOTTOM: windowtext 0.5pt solid; " +
             "BORDER-LEFT: windowtext 0.5pt solid; " +
             "BACKGROUND-COLOR: yellow";
+
+        private const string STYLE_CONTENT =
+            "HEIGHT: 12pt; " +
+            "BORDER-TOP: windowtext; " +
+            "BORDER-RIGHT: windowtext 0.5pt solid; " +
+            "BORDER-BOTTOM: windowtext 0.5pt solid; " +
+            "BORDER-LEFT: windowtext 0.5pt solid; " +
+            "BACKGROUND-COLOR: transparent";
+
         public GroupUpEmails()
         {
             InitializeComponent();
+            txtSender.Text = UserPreference.Instance.Data.UserEmail;
+            txtPwd.Text = UserPreference.Instance.Data.UserEmailPassword;
+            /*
+            if ((txtRecieverFile.Text = UserPreference.Instance.Data.RecieverDataPath) != "") {
+                GetRecievers(txtRecieverFile.Text);
+            }*/
+
             btnReciever.Click += (sender, e) =>{
                 GetRecieverFilePath();
                 GetRecievers(txtRecieverFile.Text);
@@ -40,7 +53,6 @@ namespace GropUpEmails
 
             btnOK.Click += (sender, e) => {
                 GetMailSend(recieverGridView.SelectedRows[0].Cells[3].Value.ToString());
-                //GetAndWrite(txtDataFile.Text, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)+"\\123.xml");
             };
             regenarateBtn.Click += (sender, e) => {
                 GenerateDataPreview(txtDataFile.Text);
@@ -50,13 +62,16 @@ namespace GropUpEmails
                     e.Cancel = true;
                     WindowState = FormWindowState.Minimized;
                     ShowInTaskbar = false;
+                    UserPreference.Instance.Data.UserEmail = txtSender.Text;
+                    UserPreference.Instance.Data.UserEmailPassword = txtPwd.Text;
+                    UserPreference.Instance.Data.RecieverDataPath = txtRecieverFile.Text ;
+                    UserPreference.Instance.Save();
                     Application.Exit();
                 }
                 else {
                     e.Cancel = true;
                 }
             };
-            
         }
         private void GetRecievers(string xlsFilePath) {
             try
@@ -125,8 +140,6 @@ namespace GropUpEmails
                 return;
             try {
                 Stream myStream = openFileDialog.OpenFile();
-                using (myStream) {
-                }
                 txtDataFile.Text = openFileDialog.FileName;
             }
             catch (Exception e) {
@@ -141,7 +154,7 @@ namespace GropUpEmails
             {
                 progressBar.Value = 1;
                 string strConn = $"Provider=Microsoft.Ace.OleDb.12.0;Data Source={xlsFilePath};Extended Properties='Excel 12.0; HDR=Yes; IMEX=1'";
-                string strComm = $"SELECT 教师姓名,教师证件号,年级,科目,实际单价,课时,学生姓名,班主任,教学点 FROM [Sheet1$] WHERE 教师证件号 = \"{recieverGridView.SelectedRows[0].Cells[2].Value}\" ";
+                string strComm = $"SELECT * FROM [Sheet1$] WHERE 教师证件号 = \"{recieverGridView.SelectedRows[0].Cells[2].Value}\" ";
                 OleDbConnection myConn = new OleDbConnection(strConn);
                 OleDbDataAdapter myAdp = new OleDbDataAdapter(strComm, strConn);
                 DataSet ds = new DataSet();
@@ -229,7 +242,7 @@ namespace GropUpEmails
                     foreach (var column in data.Columns) {
                         XmlNode td = xml.CreateElement("td");
                         td.Attributes.Append(createAttribute(xml, "class", "xl23212"));
-                        td.Attributes.Append(createAttribute(xml, "style", STYLE_TITLE));
+                        td.Attributes.Append(createAttribute(xml, "style", STYLE_HEADER));
                         td.Attributes.Append(createAttribute(xml, "height", "30"));
                         td.Attributes.Append(createAttribute(xml, "width", "123"));
                         XmlNode strong = xml.CreateElement("strong");
@@ -253,7 +266,7 @@ namespace GropUpEmails
                     {
                         XmlNode td = xml.CreateElement("td");
                         td.Attributes.Append(createAttribute(xml, "class", "xl23212"));
-                        td.Attributes.Append(createAttribute(xml, "style", "BORDER-TOP: windowtext; HEIGHT: 12pt; BORDER-RIGHT: windowtext 0.5pt solid; BORDER-BOTTOM: windowtext 0.5pt solid; BORDER-LEFT: windowtext 0.5pt solid; BACKGROUND-COLOR: transparent"));
+                        td.Attributes.Append(createAttribute(xml, "style", STYLE_CONTENT));
                         td.Attributes.Append(createAttribute(xml, "height", "16"));
                         td.Attributes.Append(createAttribute(xml, "width", "123"));
                         XmlNode font = xml.CreateElement("font");
@@ -275,86 +288,6 @@ namespace GropUpEmails
                 MessageBox.Show(e.Message);
                 throw;
             }
-
-
-            /*
-            content = table.Columns.Cast<DataColumn>().Aggregate("", (current, dc) => current + dc.ColumnName.PadRight(10));
-            txtContent.Text += Resources.Endline;
-            foreach (DataRow row in table.Rows) {
-                for (int i = 0; i < table.Columns.Count; i++)
-                    content += row[i].ToString().PadRight(10);
-                content += Resources.Endline;
-            }*/
-        }
-
-        public void GetAndWrite(string strFileName, string strDesPath) {
-            Microsoft.Office.Interop.Excel.Application excelRead = new ApplicationClass();
-            Workbooks workbooks = excelRead.Workbooks;
-            object mo = Missing.Value;
-            Workbook workbook = workbooks.Open(strFileName, mo, mo, mo, mo, mo, mo, mo, mo, mo, mo, mo, mo, mo, mo);
-            string[] strSheetNames = {"Sheet1$"};
-
-            Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
-            int bgnRow = (worksheet.UsedRange.Cells.Row > 1) ? worksheet.UsedRange.Cells.Row - 1 : worksheet.UsedRange.Cells.Row;
-            int bgnColumn = (worksheet.UsedRange.Cells.Column > 1) ? worksheet.UsedRange.Cells.Column - 1 : worksheet.UsedRange.Cells.Column;
-            //设置保存xml文件
-            string strFile = strDesPath.Substring(strDesPath.LastIndexOf("\\", StringComparison.Ordinal) + 1);
-            strFile = strFile.Remove(strFile.IndexOf(".", StringComparison.Ordinal), 4);
-
-            string strPath = strDesPath.Substring(0, strDesPath.LastIndexOf("\\", StringComparison.Ordinal));
-            string fileName = strPath + "\\" + strFile + ".xml";
-            
-            XmlTextWriter writexml = new XmlTextWriter(fileName, Encoding.Default);
-            writexml.Formatting = Formatting.Indented;
-            writexml.WriteStartDocument();
-            writexml.WriteStartElement("workbook");
-            writexml.WriteStartElement("worksheet");
-            writexml.WriteAttributeString("name", strSheetNames[0]);
-            writexml.WriteStartElement("table");
-            writexml.WriteAttributeString("sumRow", worksheet.UsedRange.Cells.Rows.Count.ToString());
-            writexml.WriteAttributeString("sumCol", worksheet.UsedRange.Cells.Columns.Count.ToString());
-
-            for (int i = bgnRow; i < worksheet.UsedRange.Cells.Rows.Count + bgnRow; ++i) {
-                writexml.WriteStartElement("row");
-                for (int j = bgnColumn; j < worksheet.UsedRange.Cells.Columns.Count + bgnColumn; ++j) {
-                    string strData = ((Range)worksheet.UsedRange.Cells[i, j]).Text.ToString();
-                    int mergeAcross = ((Range)worksheet.Cells[i, j]).MergeArea.Count;
-                    mergeAcross -= 1;
-                    string backgroundColor = ((Range)worksheet.Cells[i, j]).Interior.Color.ToString();
-                    Int32 bgcolor = Int32.Parse(backgroundColor);
-                    Color backClor = Color.FromArgb(bgcolor);
-                    string Bgcolor = ColorTranslator.ToHtml(backClor);
-
-                    string fontFamily = ((Range)worksheet.Cells[i, j]).Font.Name.ToString();
-
-                    string fontColor = ((Range)worksheet.Cells[i, j]).Font.Color.ToString();
-                    int fcolor = int.Parse(fontColor);
-                    Color FontColor = Color.FromArgb(fcolor);
-                    string font_color = ColorTranslator.ToHtml(FontColor);
-
-                    string fontSize = ((Range)worksheet.Cells[i, j]).Font.Size.ToString();
-                    if (mergeAcross > 1) {
-                        j += mergeAcross;
-                    }
-                    writexml.WriteStartElement("cell");
-                    writexml.WriteAttributeString("isMerge", mergeAcross.ToString());
-                    if ((backgroundColor == "16777215") && (Equals(fontFamily, "宋体")) && (fontColor == "0") && (fontSize == "12")) {
-                        writexml.WriteAttributeString("style", "");
-                    }
-                    else {
-                        writexml.WriteAttributeString("style", " background-color:" + Bgcolor + ";" + "font-family:" + fontFamily + ";" + "font-color:" + font_color + ";" + "font-size:" + fontSize + ";");
-                    }
-                    writexml.WriteString(strData);
-                    writexml.WriteEndElement();
-                }
-                writexml.WriteEndElement();
-            }
-            writexml.WriteEndElement();
-            writexml.WriteEndElement();
-            writexml.WriteEndElement();
-            writexml.WriteEndDocument();
-            writexml.Close();
-            excelRead.Quit();
         }
     }
 }
---
+++ GropUpEmails/UserPreference.cs
@@ -0,0 +1,40 @@
+﻿
+using System.IO;
+
+namespace GropUpEmails {
+    public class UserPreferenceData {
+        public string UserEmail="";
+        public string UserEmailPassword="";
+        public string RecieverDataPath = "";
+    }
+    class UserPreference
+    {
+        private UserPreference()
+        {
+            Load();
+        }
+
+        private static UserPreference _instance;
+        public static UserPreference Instance => _instance ?? (_instance = new UserPreference());
+
+        private readonly string _dataFileName = Directory.GetCurrentDirectory() + "\\user.dat";//存档文件的名称,自己定//
+        private readonly XmlSaver _xmlSaver = new XmlSaver();
+
+        public UserPreferenceData Data;
+
+        public void Save() {
+            string dataString = _xmlSaver.SerializeObject(Data ?? (Data = new UserPreferenceData()), typeof(UserPreferenceData));
+            _xmlSaver.CreateXML(_dataFileName, dataString);
+        }
+
+        private void Load() {
+            if (File.Exists(_dataFileName)) {
+                string dataString = _xmlSaver.LoadXML(_dataFileName);
+                Data = _xmlSaver.DeserializeObject(dataString, typeof(UserPreferenceData)) as UserPreferenceData;
+            }
+            else {
+                 Save();
+            }
+        }
+    }
+}
